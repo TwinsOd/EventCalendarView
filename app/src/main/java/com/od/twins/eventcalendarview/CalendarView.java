bc065ddacc6150ef17638b11db7e9111de67d98f
Zakharov <vladymyr.zakharov@ab-soft.net>
2017-10-30 14:17:33 +0200

    add event backgrounds

--- app/src/main/java/com/od/twins/eventcalendarview/CalendarView.java
+++ app/src/main/java/com/od/twins/eventcalendarview/CalendarView.java
@@ -5,6 +5,7 @@ import android.content.res.TypedArray;
 import android.graphics.Color;
 import android.graphics.Typeface;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -25,26 +26,17 @@ import java.util.HashSet;
  * Created by user on 10/27/2017.
  */
 
-public class CalendarView  extends LinearLayout
-{
-    // for logging
-    private static final String LOGTAG = "Calendar View";
-
+public class CalendarView extends LinearLayout {
     // how many days to show, defaults to six weeks, 42 days
     private static final int DAYS_COUNT = 42;
-
     // default date format
     private static final String DATE_FORMAT = "MMM yyyy";
-
     // date format
     private String dateFormat;
-
     // current displayed month
     private Calendar currentDate = Calendar.getInstance();
-
     //event handling
     private EventHandler eventHandler = null;
-
     // internal components
     private LinearLayout header;
     private ImageView btnPrev;
@@ -53,7 +45,7 @@ public class CalendarView  extends LinearLayout
     private GridView grid;
 
     // seasons' rainbow
-    int[] rainbow = new int[] {
+    int[] rainbow = new int[]{
             R.color.summer,
             R.color.fall,
             R.color.winter,
@@ -61,21 +53,18 @@ public class CalendarView  extends LinearLayout
     };
 
     // month-season association (northern hemisphere, sorry australia :)
-    int[] monthSeason = new int[] {2, 2, 3, 3, 3, 0, 0, 0, 1, 1, 1, 2};
+    int[] monthSeason = new int[]{2, 2, 3, 3, 3, 0, 0, 0, 1, 1, 1, 2};
 
-    public CalendarView(Context context)
-    {
+    public CalendarView(Context context) {
         super(context);
     }
 
-    public CalendarView(Context context, AttributeSet attrs)
-    {
+    public CalendarView(Context context, AttributeSet attrs) {
         super(context, attrs);
         initControl(context, attrs);
     }
 
-    public CalendarView(Context context, AttributeSet attrs, int defStyleAttr)
-    {
+    public CalendarView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         initControl(context, attrs);
     }
@@ -83,8 +72,7 @@ public class CalendarView  extends LinearLayout
     /**
      * Load control xml layout
      */
-    private void initControl(Context context, AttributeSet attrs)
-    {
+    private void initControl(Context context, AttributeSet attrs) {
         LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         inflater.inflate(R.layout.control_calendar, this);
 
@@ -95,69 +83,68 @@ public class CalendarView  extends LinearLayout
         updateCalendar();
     }
 
-    private void loadDateFormat(AttributeSet attrs)
-    {
+    private void loadDateFormat(AttributeSet attrs) {
         TypedArray ta = getContext().obtainStyledAttributes(attrs, R.styleable.CalendarView);
 
-        try
-        {
+        try {
             // try to load provided date format, and fallback to default otherwise
             dateFormat = ta.getString(R.styleable.CalendarView_dateFormat);
             if (dateFormat == null)
                 dateFormat = DATE_FORMAT;
-        }
-        finally
-        {
+        } finally {
             ta.recycle();
         }
     }
-    private void assignUiElements()
-    {
+
+    private void assignUiElements() {
         // layout is inflated, assign local variables to components
-        header = (LinearLayout)findViewById(R.id.calendar_header);
-        btnPrev = (ImageView)findViewById(R.id.calendar_prev_button);
-        btnNext = (ImageView)findViewById(R.id.calendar_next_button);
-        txtDate = (TextView)findViewById(R.id.calendar_date_display);
-        grid = (GridView)findViewById(R.id.calendar_grid);
+        header = (LinearLayout) findViewById(R.id.calendar_header);
+        btnPrev = (ImageView) findViewById(R.id.calendar_prev_button);
+        btnNext = (ImageView) findViewById(R.id.calendar_next_button);
+        txtDate = (TextView) findViewById(R.id.calendar_date_display);
+        grid = (GridView) findViewById(R.id.calendar_grid);
     }
 
-    private void assignClickHandlers()
-    {
+    private void assignClickHandlers() {
         // add one month and refresh UI
-        btnNext.setOnClickListener(new OnClickListener()
-        {
+        btnNext.setOnClickListener(new OnClickListener() {
             @Override
-            public void onClick(View v)
-            {
+            public void onClick(View v) {
                 currentDate.add(Calendar.MONTH, 1);
                 updateCalendar();
             }
         });
 
         // subtract one month and refresh UI
-        btnPrev.setOnClickListener(new OnClickListener()
-        {
+        btnPrev.setOnClickListener(new OnClickListener() {
             @Override
-            public void onClick(View v)
-            {
+            public void onClick(View v) {
                 currentDate.add(Calendar.MONTH, -1);
                 updateCalendar();
             }
         });
 
         // long-pressing a day
-        grid.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener()
-        {
+//        grid.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+//
+//            @Override
+//            public boolean onItemLongClick(AdapterView<?> view, View cell, int position, long id) {
+//                // handle long-press
+//                if (eventHandler == null)
+//                    return false;
+//
+//                eventHandler.onDayLongPress((Date) view.getItemAtPosition(position));
+//                return true;
+//            }
+//        });
 
+        grid.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public boolean onItemLongClick(AdapterView<?> view, View cell, int position, long id)
-            {
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Log.d("CalendarView", "Date click : " + i);
                 // handle long-press
-                if (eventHandler == null)
-                    return false;
-
-                eventHandler.onDayLongPress((Date)view.getItemAtPosition(position));
-                return true;
+                if (eventHandler != null)
+                    eventHandler.onDayLongPress((Date) adapterView.getItemAtPosition(i));
             }
         });
     }
@@ -165,18 +152,16 @@ public class CalendarView  extends LinearLayout
     /**
      * Display dates correctly in grid
      */
-    public void updateCalendar()
-    {
+    public void updateCalendar() {
         updateCalendar(null);
     }
 
     /**
      * Display dates correctly in grid
      */
-    public void updateCalendar(HashSet<Date> events)
-    {
+    public void updateCalendar(HashSet<Date> events) {
         ArrayList<Date> cells = new ArrayList<>();
-        Calendar calendar = (Calendar)currentDate.clone();
+        Calendar calendar = (Calendar) currentDate.clone();
 
         // determine the cell for current month's beginning
         calendar.set(Calendar.DAY_OF_MONTH, 1);
@@ -186,8 +171,7 @@ public class CalendarView  extends LinearLayout
         calendar.add(Calendar.DAY_OF_MONTH, -monthBeginningCell);
 
         // fill cells
-        while (cells.size() < DAYS_COUNT)
-        {
+        while (cells.size() < DAYS_COUNT) {
             cells.add(calendar.getTime());
             calendar.add(Calendar.DAY_OF_MONTH, 1);
         }
@@ -208,24 +192,22 @@ public class CalendarView  extends LinearLayout
     }
 
 
-    private class CalendarAdapter extends ArrayAdapter<Date>
-    {
+    private class CalendarAdapter extends ArrayAdapter<Date> {
         // days with events
         private HashSet<Date> eventDays;
 
         // for view inflation
         private LayoutInflater inflater;
 
-        public CalendarAdapter(Context context, ArrayList<Date> days, HashSet<Date> eventDays)
-        {
+        public CalendarAdapter(Context context, ArrayList<Date> days, HashSet<Date> eventDays) {
             super(context, R.layout.control_calendar_day, days);
             this.eventDays = eventDays;
             inflater = LayoutInflater.from(context);
         }
 
         @Override
-        public View getView(int position, View view, ViewGroup parent)
-        {
+        public View getView(int position, View viewParent, ViewGroup parent) {
+
             // day in question
             Date date = getItem(position);
             int day = date.getDate();
@@ -236,54 +218,53 @@ public class CalendarView  extends LinearLayout
             Date today = new Date();
 
             // inflate item if it does not exist yet
-            if (view == null)
-                view = inflater.inflate(R.layout.control_calendar_day, parent, false);
+            if (viewParent == null)
+                viewParent = inflater.inflate(R.layout.control_calendar_day, parent, false);
 
+            TextView numberView = viewParent.findViewById(R.id.number_view);
             // if this day has an event, specify event image
-            view.setBackgroundResource(0);
-            if (eventDays != null)
-            {
-                for (Date eventDate : eventDays)
-                {
-                    if (eventDate.getDate() == day &&
-                            eventDate.getMonth() == month &&
-                            eventDate.getYear() == year)
-                    {
+            numberView.setBackgroundResource(0);
+            // clear styling
+            numberView.setTypeface(null, Typeface.NORMAL);
+            numberView.setTextColor(Color.BLACK);
+
+            if (month != today.getMonth() || year != today.getYear()) {
+                // if this day is outside current month, grey it out
+                numberView.setTextColor(getResources().getColor(R.color.greyed_out));
+            }
+
+            if (day == today.getDate()) {
+                // if it is today, set it to blue/bold
+                numberView.setTypeface(null, Typeface.BOLD);
+                numberView.setTextColor(getResources().getColor(R.color.today));
+                numberView.setBackgroundResource(R.drawable.background_today);
+            }
+
+            if (eventDays != null) {
+                for (Date eventDate : eventDays) {
+                    if (eventDate.getDate() == day && eventDate.getMonth() == month && eventDate.getYear() == year) {
                         // mark this day for event
-                        view.setBackgroundResource(R.drawable.reminder);
+                        if (eventDate.getDate() == today.getDate()) {
+                            numberView.setBackgroundResource(R.drawable.background_event_today);
+                            numberView.setTextColor(getResources().getColor(R.color.today));
+                        } else
+                            numberView.setBackgroundResource(R.drawable.background_event);
                         break;
                     }
                 }
             }
 
-            // clear styling
-            ((TextView)view).setTypeface(null, Typeface.NORMAL);
-            ((TextView)view).setTextColor(Color.BLACK);
-
-            if (month != today.getMonth() || year != today.getYear())
-            {
-                // if this day is outside current month, grey it out
-                ((TextView)view).setTextColor(getResources().getColor(R.color.greyed_out));
-            }
-            else if (day == today.getDate())
-            {
-                // if it is today, set it to blue/bold
-                ((TextView)view).setTypeface(null, Typeface.BOLD);
-                ((TextView)view).setTextColor(getResources().getColor(R.color.today));
-            }
-
             // set text
-            ((TextView)view).setText(String.valueOf(date.getDate()));
+            numberView.setText(String.valueOf(date.getDate()));
 
-            return view;
+            return numberView;
         }
     }
 
     /**
      * Assign event handler to be passed needed events
      */
-    public void setEventHandler(EventHandler eventHandler)
-    {
+    public void setEventHandler(EventHandler eventHandler) {
         this.eventHandler = eventHandler;
     }
 
@@ -291,8 +272,7 @@ public class CalendarView  extends LinearLayout
      * This interface defines what events to be reported to
      * the outside world
      */
-    public interface EventHandler
-    {
+    public interface EventHandler {
         void onDayLongPress(Date date);
     }
 }
--- app/src/main/java/com/od/twins/eventcalendarview/MainActivity.java
+++ app/src/main/java/com/od/twins/eventcalendarview/MainActivity.java
@@ -1,7 +1,8 @@
 package com.od.twins.eventcalendarview;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
@@ -20,45 +21,50 @@ public class MainActivity extends AppCompatActivity {
 
         HashSet<Date> events = new HashSet<>();
         events.add(new Date());
+        Date date1 = new Date();
+        date1.setDate(23);
+        events.add(date1);
+        Date date2 = new Date();
+        date2.setDate(15);
+        events.add(date2);
+        Date date3 = new Date();
+        date3.setDate(6);
+        events.add(date3);
 
         CalendarView cv = findViewById(R.id.calendar_view);
         cv.updateCalendar(events);
 
         // assign event handler
-        cv.setEventHandler(new CalendarView.EventHandler()
-        {
+        cv.setEventHandler(new CalendarView.EventHandler() {
             @Override
-            public void onDayLongPress(Date date)
-            {
+            public void onDayLongPress(Date date) {
                 // show returned day
                 DateFormat df = SimpleDateFormat.getDateInstance();
                 Toast.makeText(MainActivity.this, df.format(date), Toast.LENGTH_SHORT).show();
+                Log.d("MainActivity", "Date click : " + df.format(date));
             }
         });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu)
-    {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item)
-    {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings)
-        {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.menu_main, menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        int id = item.getItemId();
+//
+//        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+//
+//        return super.onOptionsItemSelected(item);
+//    }
 }
